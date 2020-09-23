b105d6c137dae2c72a90e6d4651a6cbfe1bf1738
Ubuntu <ubuntu@ip-172-31-28-119.ec2.internal>
2020-09-23 13:28:24 +0000

    update

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nxs1v34%+5_et^wbr7bp!9fqv9)seb39tat&hhe)37!_!c!_6!'
+SECRET_KEY =''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -78,9 +78,9 @@ DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sitedata',
-        'USER':'root',
+        'USER':'admin',
         'PASSWORD':'',
-        'HOST':'localhost',
+        'HOST':'awstest.cb6vn4kmxe1s.us-east-1.rds.amazonaws.com',
         'PORT':'3306'
     }
 }
